8b55d30b4eef3a79fec9aa8f7aaa3c36fefd050c
dieptonn <diep.bt194505@sis.hust.edu.vn>
2023-07-19 21:10:36 +0700

    completed

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-var express = require("express");
-var app = express();
-var server = require("http").createServer(app);
-var io = require("socket.io")(server);
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
 app.get("/", function(req, res, next) {
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/src/index.html");
 });
 
-app.use(express.static("public"));
+app.use(express.static("src"));
 
-io.on("connection", function(client) {
-  console.log("Client connected...");
+io.on("connection", function(socket) {
+  console.log("client connected...");
 
-  client.on("join", function(data) {
+  socket.on("join", function(data) {
     console.log(data);
   });
 
-  client.on("messages", function(data) {
-    client.emit("thread", data);
-    client.broadcast.emit("thread", data);
+  socket.on("messages", function(data) {
+    socket.emit("thread", data);
+    socket.broadcast.emit("thread", data);
   });
 });
 
